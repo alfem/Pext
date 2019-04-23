a9093ae2120c0ace654f83b762ddca9f35395cc5
Allan Nordhøy <epost@anotheragency.no>
2019-04-23 12:05:17 +0000

    Translated using Weblate (Norwegian Bokmål)
    Currently translated at 92.9% (171 of 184 strings)
    
    Translation: Pext/Pext
    Translate-URL: https://hosted.weblate.org/projects/pext/pext/nb_NO/

--- pext/i18n/pext_nb_NO.ts
+++ pext/i18n/pext_nb_NO.ts
@@ -625,7 +625,7 @@ Er du sikker på at du vil avslutte?</translation>
     <message>
         <location filename="../qml/main.qml" line="625"/>
         <source>Turbo Mode</source>
-        <translation type="unfinished"></translation>
+        <translation>Turbo-modus</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="633"/>
@@ -870,7 +870,7 @@ Er du sikker på at du vil avslutte?</translation>
     <message>
         <location filename="../qml/main.qml" line="992"/>
         <source>Pynput is unavailable</source>
-        <translation type="unfinished"></translation>
+        <translation>Pynput er utilgjengelig</translation>
     </message>
     <message>
         <location filename="../qml/main.qml" line="425"/>
